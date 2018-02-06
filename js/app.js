58a2b6f85c6d9ebb23c1aa1be2d47182020761a3
Brett <brettspencr@gmail.com>
2018-02-06 14:33:15 -0800

    refactored to allow arbitrary map sizes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
+// LATER
+// Fix walking bug:
+// https://stackoverflow.com/questions/29279805/keydown-doesnt-continuously-fire-when-pressed-and-hold?rq=1
+
+// TODAY
+// add buffer rows + columns around maps, move map transition squares back
+// add jump behavior for ledges
+// add objects + NPCs to maps
+// update maps.js --> JSON, start that server thing, load data using AJAX
+// add npcs data to dedicated
+// make map transitions smoother
+
 var squareSize = 2.5;
-var playerY = 6; //map y = ((5 - playerY) * squareSize) + "vw";
-var playerX = 5; //map x = ((5 - playerX) * squareSize) + "vw";
+var playerY = 7; 
+var playerX = 6;
 var playerState = "standing";
 var playerDirection = "down";
 var walkingInterval = null;
 var moving = false;
 var token = 0;
 var currentLocation = "pallet";
+var mapWidth = mapLocations[currentLocation][0][1].length;
+var mapHeight = mapLocations[currentLocation][0].length;
 
 function movePlayer(direction) {
     if (!moving) {
@@ -18,7 +32,7 @@ function movePlayer(direction) {
                 targetSquare = mapLocations[currentLocation][0][playerY - 1][playerX];
                 if (typeof targetSquare === "string") {
                     loadNewMapArea(targetSquare);
-                } else if (playerY > 0 && targetSquare === 0) {
+                } else if (playerY > 0 && targetSquare !== 1) {
                     playerY--
                 }
                 break;
@@ -27,7 +41,9 @@ function movePlayer(direction) {
                 targetSquare = mapLocations[currentLocation][0][playerY + 1][playerX];
                 if (typeof targetSquare === "string") {
                     loadNewMapArea(targetSquare);
-                } else if (playerY < 18 && targetSquare === 0) {
+                } else if (targetSquare === 4) {
+                    triggerFight();
+                } else if (playerY < mapHeight && targetSquare !== 1) {
                     playerY++
                 }
                 break;
@@ -36,7 +52,7 @@ function movePlayer(direction) {
                 targetSquare = mapLocations[currentLocation][0][playerY][playerX - 1];
                 if (typeof targetSquare === "string") {
                     loadNewMapArea(targetSquare);
-                } else if (playerX > 0 && targetSquare === 0) {
+                } else if (playerX > 0 && targetSquare !== 1) {
                     playerX--
                 }
                 break;
@@ -45,17 +61,17 @@ function movePlayer(direction) {
                 targetSquare = mapLocations[currentLocation][0][playerY][playerX + 1];
                 if (typeof targetSquare === "string") {
                     loadNewMapArea(targetSquare);
-                } else if (playerX < 20 && targetSquare === 0) {
+                } else if (playerX < mapWidth && targetSquare !== 1) {
                     playerX++
                 }
                 break;
             default:
         };
-        var mapY = ((5 - playerY) * squareSize) + "vw";
-        var mapX = ((5 - playerX) * squareSize) + "vw";
+        var mapY = ((6 - playerY) * squareSize) + "vw";  // (5 squares on screen above + offset by 1 buffer row) - playerY
+        var mapX = ((6 - playerX) * squareSize) + "vw";  // (5 squares on screen above + offset by 1 buffer col) - playerX
         $("#screen").css("background-position", mapX + " " + mapY);
         moving = true;
-        setTimeout(function(){ moving = false }, 240)
+        setTimeout(function(){ moving = false }, 150)
         playerState = "walking";
         animatePlayerMovement(direction);
     }
@@ -64,12 +80,15 @@ function movePlayer(direction) {
 function loadNewMapArea(name) {
     var lastLocation = currentLocation;
     currentLocation = name;
+    mapWidth = mapLocations[currentLocation][0][1].length - 2;
+    mapHeight = mapLocations[currentLocation][0].length - 2;
     $("#player").hide();
     $("#screen").css("background-image", "url(img/" + name + ".png)");
+    $("#screen").css("background-size", (mapWidth * squareSize) + "vw " + (mapHeight * squareSize) + "vw");
     playerY = mapLocations[currentLocation][1][lastLocation][0];
     playerX = mapLocations[currentLocation][1][lastLocation][1];
-    var mapY = ((5 - playerY) * squareSize) + "vw";
-    var mapX = ((5 - playerX) * squareSize) + "vw";
+    var mapY = ((6 - playerY) * squareSize) + "vw";
+    var mapX = ((6 - playerX) * squareSize) + "vw";
     $("#screen").css("background-position", mapX + " " + mapY);
     setTimeout(function() {
         $("#player").show();
@@ -109,12 +128,16 @@ function animatePlayerMovement(direction) {
                 $("#player").css("background-position", spriteX + " " + spriteY2);
                 token = 0;
             }   
-        }, 250);
+        }, 150);
     } 
 }
 
 function buttonPress(button) {
     console.log(button, "was pressed");
+}
+
+function triggerFight() {
+    console.log("This square can trigger a fight");
 }
 
 $(document).ready(function() {
@@ -169,11 +192,14 @@ $(document).ready(function() {
                 buttonPress("start");
                 break;
             default:
-        };
+        };     
     });
-    //Need to only trigger this if a move key was hit... right now it fires for any key
-    $(document).on("keyup", function() {
-        playerState = "standing";
-        animatePlayerMovement(playerDirection);
+
+    $(document).on("keyup", function(event) {
+        var gameButtons = [37, 65, 38, 87, 39, 68, 40, 83, 75, 76, 71, 72];
+        if (gameButtons.includes(event.keyCode)) {
+            playerState = "standing";
+            animatePlayerMovement(playerDirection);
+        }
     });
 });
